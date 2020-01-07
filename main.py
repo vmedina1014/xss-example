af9004a81a6a2006df9db24bebaf2e326a3335ff
Victor A Medina <vmedina1014@gmail.com>
2020-01-06 19:23:12 -0800

    Add files via upload

--- main.py
+++ main.py
@@ -22,7 +22,6 @@ def home():
     <textarea name="content"></textarea>
     <input type="submit" value="Submit">
 </form>
-
 <h2>Wisdom From Your Fellow Classmates</h2>
 """
     
@@ -31,7 +30,7 @@ def home():
 <div class="message">
 {}
 </div>
-""".format(m.content)
+""".format(m.content.replace('<', '&lt;').replace('>', '&gt;'))
 
     return body 
 
@@ -39,4 +38,3 @@ def home():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 6738))
     app.run(host='0.0.0.0', port=port)
-
